4ea74ced28ccb2ac3baec3a725692c13c72a38da
begmaroman <begmaroman@gmail.com>
2023-11-24 19:47:47 +0800

    Addressing comments

--- pkg/network/network.go
+++ pkg/network/network.go
@@ -5,23 +5,20 @@ import (
 	"net"
 )
 
-type IPBinding string
-
 const (
-	LocalHostBinding     IPBinding = "127.0.0.1"
-	AllInterfacesBinding IPBinding = "0.0.0.0"
+	AllInterfacesBinding = "0.0.0.0"
 )
 
 // ResolveAddr resolves the passed in TCP address
 // The second param is the default ip to bind to, if no ip address is specified
-func ResolveAddr(address string, defaultIP IPBinding) (*net.TCPAddr, error) {
+func ResolveAddr(address string, defaultIp string) (*net.TCPAddr, error) {
 	addr, err := net.ResolveTCPAddr("tcp", address)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse addr '%s': %w", address, err)
 	}
 
 	if addr.IP == nil {
-		addr.IP = net.ParseIP(string(defaultIP))
+		addr.IP = net.ParseIP(defaultIp)
 	}
 
 	return addr, nil
