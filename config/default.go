0ea4d6e7e8c9d3d361e9786d7eecb95e8f8cd109
begmaroman <begmaroman@gmail.com>
2024-01-30 14:03:24 +0000

    Added RollupManagerContract

--- config/default.go
+++ config/default.go
@@ -10,45 +10,46 @@ import (
 // DefaultValues is the default configuration
 const DefaultValues = `
 [FullNodeRPCs]
-1 = "http://zkevm-node:8123"
+	1 = "http://zkevm-node:8123"
 
 [RPC]
-Host = "0.0.0.0"
-Port = 4444
-ReadTimeout = "60s"
-WriteTimeout = "60s"
-MaxRequestsPerIPAndSecond = 5000
+	Host = "0.0.0.0"
+	Port = 4444
+	ReadTimeout = "60s"
+	WriteTimeout = "60s"
+	MaxRequestsPerIPAndSecond = 5000
 
 [Log]
-Environment = "development" # "production" or "development"
-Level = "debug"
-Outputs = ["stderr"]
+	Environment = "development" # "production" or "development"
+	Level = "debug"
+	Outputs = ["stderr"]
 
 [DB]
-User = "agglayer_user"
-Password = "agglayer_password"
-Name = "agglayer_db"
-Host = "agglayer-db"
-Port = "5432"
-EnableLog = false
-MaxConns = 200
+	User = "agglayer_user"
+	Password = "agglayer_password"
+	Name = "agglayer_db"
+	Host = "agglayer-db"
+	Port = "5432"
+	EnableLog = false
+	MaxConns = 200
 
 [EthTxManager]
-FrequencyToMonitorTxs = "1s"
-WaitTxToBeMined = "2m"
-ForcedGas = 0
-GasPriceMarginFactor = 1
-MaxGasPriceLimit = 0
-PrivateKeys = [
-	{Path = "/pk/agglayer.keystore", Password = "testonly"},
-]
+	FrequencyToMonitorTxs = "1s"
+	WaitTxToBeMined = "2m"
+	ForcedGas = 0
+	GasPriceMarginFactor = 1
+	MaxGasPriceLimit = 0
+	PrivateKeys = [
+		{Path = "/pk/agglayer.keystore", Password = "testonly"},
+	]
 
 [L1]
-ChainID = 1337
-NodeURL = "http://l1:8545"
+	ChainID = 1337
+	NodeURL = "http://l1:8545"
+	RollupManagerContract = "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e" # v0.0.4
 
 [Telemetry]
-PrometheusAddr = "0.0.0.0:2223"
+	PrometheusAddr = "0.0.0.0:2223"
 `
 
 // Default parses the default configuration values.
