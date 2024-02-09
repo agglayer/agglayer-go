adf641db13d7f0a1ce6cb4dc64c97f9726e6fb07
Victor Castell <victor@polygon.technology>
2024-02-09 12:56:29 +0100

    tmp

--- etherman/etherman_test.go
+++ etherman/etherman_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/0xPolygon/agglayer/config"
 	cdkTypes "github.com/0xPolygon/agglayer/rpc/types"
 	"github.com/0xPolygon/agglayer/tx"
+	"github.com/0xPolygonHermez/zkevm-node/etherman/smartcontracts/polygonrollupmanager"
 	"github.com/ethereum/go-ethereum/crypto"
 
 	"github.com/0xPolygon/agglayer/mocks"
@@ -42,6 +43,48 @@ func getEtherman(ethClientMock IEthereumClient) Etherman {
 	)
 
 	return ethman
+}
+
+func Test_BuildTrustedVerifyBatchesTxData(t *testing.T) {
+	lastVerifiedBatch := uint64(0x0)
+	newVerifiedBatch := uint64(0x3)
+	proof := tx.ZKP{
+		NewStateRoot:     common.HexToHash("0x57ad8ae2377851bc1f77f16261684fb838660d66036f4b1fd1d63199f0621711"),
+		NewLocalExitRoot: common.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000"),
+		Proof:            common.Hex2Bytes("0x20227cbcef731b6cbdc0edd5850c63dc7fbc27fb58d12cd4d08298799cf66a0512c230867d3375a1f4669e7267dad2c31ebcddbaccea6abd67798ceae35ae7611c665b6069339e6812d015e239594aa71c4e217288e374448c358f6459e057c91ad2ef514570b5dea21508e214430daadabdd23433820000fe98b1c6fa81d5c512b86fbf87bd7102775f8ef1da7e8014dc7aab225503237c7927c032e589e9a01a0eab9fda82ffe834c2a4977f36cc9bcb1f2327bdac5fb48ffbeb9656efcdf70d2656c328903e9fb96e4e3f470c447b3053cc68d68cf0ad317fe10aa7f254222e47ea07f3c1c3aacb74e5926a67262f261c1ed3120576ab877b49a81fb8aac51431858662af6b1a8138a44e9d0812d032340369459ccc98b109347cc874c7202dceecc3dbb09d7f9e5658f1ca3a92d22be1fa28f9945205d853e2c866d9b649301ac9857b07b92e4865283d3d5e2b711ea5f85cb2da71965382ece050508d3d008bbe4df5458f70bd3e1bfcc50b34222b43cd28cbe39a3bab6e464664a742161df99c607638e415ced49d0cd719518539ed5f561f81d07fe40d3ce85508e0332465313e60ad9ae271d580022ffca4fbe4d72d38d18e7a6e20d020a1d1e5a8f411291ab95521386fa538ddfe6a391d4a3669cc64c40f07895f031550b32f7d73205a69c214a8ef3cdf996c495e3fd24c00873f30ea6b2bfabfd38de1c3da357d1fefe203573fdad22f675cb5cfabbec0a041b1b31274f70193da8e90cfc4d6dc054c7cd26d09c1dadd064ec52b6ddcfa0cb144d65d9e131c0c88f8004f90d363034d839aa7760167b5302c36d2c2f6714b41782070b10c51c178bd923182d28502f36e19b079b190008c46d19c399331fd60b6b6bde898bd1dd0a71ee7ec7ff7124cc3d374846614389e7b5975b77c4059bc42b810673dbb6f8b951e5b636bdf24afd2a3cbe96ce8600e8a79731b4a56c697596e0bff7b73f413bdbc75069b002b00d713fae8d6450428246f1b794d56717050fdb77bbe094ac2ee6af54a153e2fb8ce1d31a86c4fdd523783b910bedf7db58a46ba6ce48ac3ca194f3cf2275e"),
+	}
+	rollupId := uint32(1)
+
+	var newLocalExitRoot [HashLength]byte
+	copy(newLocalExitRoot[:], proof.NewLocalExitRoot.Bytes())
+	var newStateRoot [HashLength]byte
+	copy(newStateRoot[:], proof.NewStateRoot.Bytes())
+	finalProof, err := ConvertProof(proof.Proof.Hex())
+	if err != nil {
+		t.Logf("error converting proof. Error: %v, Proof: %s", err, proof.Proof)
+	}
+
+	const pendStateNum uint64 = 0 // TODO hardcoded for now until we implement the pending state feature
+	abi, err := polygonrollupmanager.PolygonrollupmanagerMetaData.GetAbi()
+	if err != nil {
+		t.Logf("error geting ABI: %v, Proof: %s", err, abi)
+	}
+
+	sa := common.HexToAddress("0x95Af2Ec2577d292E47d370dA0f424481Ebd051b5")
+
+	out, _ := abi.Pack(
+		"verifyBatchesTrustedAggregator",
+		rollupId,
+		pendStateNum,
+		lastVerifiedBatch,
+		newVerifiedBatch,
+		newLocalExitRoot,
+		newStateRoot,
+		sa,
+		finalProof,
+	)
+
+	t.Log(common.Bytes2Hex(out))
 }
 
 func TestGetSequencerAddr(t *testing.T) {
