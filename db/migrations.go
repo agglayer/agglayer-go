24367de07d3ee080e62ae01c8ba986d90cb1a09a
Victor Castell <victor@polygon.technology>
2024-02-23 09:50:48 +0100

    Refactor migrations to use sql.DB

--- db/migrations.go
+++ db/migrations.go
@@ -1,11 +1,10 @@
 package db
 
 import (
+	"database/sql"
 	"embed"
 
 	"github.com/0xPolygonHermez/zkevm-node/log"
-	"github.com/jackc/pgx/v4/pgxpool"
-	"github.com/jackc/pgx/v4/stdlib"
 	migrate "github.com/rubenv/sql-migrate"
 )
 
@@ -13,24 +12,22 @@ import (
 var f embed.FS
 
 // RunMigrationsUp runs migrate-up for the given config.
-func RunMigrationsUp(pg *pgxpool.Pool) error {
+func RunMigrationsUp(db *sql.DB) error {
 	log.Info("running migrations up")
-	return runMigrations(pg, migrate.Up)
+	return runMigrations(db, migrate.Up)
 }
 
 // RunMigrationsDown runs migrate-down for the given config.
-func RunMigrationsDown(pg *pgxpool.Pool) error {
+func RunMigrationsDown(db *sql.DB) error {
 	log.Info("running migrations down")
-	return runMigrations(pg, migrate.Down)
+	return runMigrations(db, migrate.Down)
 }
 
 // runMigrations will execute pending migrations if needed to keep
 // the database updated with the latest changes in either direction,
 // up or down.
-func runMigrations(pg *pgxpool.Pool, direction migrate.MigrationDirection) error {
-	db := stdlib.OpenDB(*pg.Config().ConnConfig)
-
-	var migrations = &migrate.EmbedFileSystemMigrationSource{FileSystem: f, Root: "migrations"}
+func runMigrations(db *sql.DB, direction migrate.MigrationDirection) error {
+	migrations := &migrate.EmbedFileSystemMigrationSource{FileSystem: f, Root: "migrations"}
 	nMigrations, err := migrate.Exec(db, "postgres", migrations, direction)
 	if err != nil {
 		return err
