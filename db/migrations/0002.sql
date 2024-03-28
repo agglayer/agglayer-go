4efe478f3a5ddeb041ca11816e79d780f0e4f21b
Goran Rojovic <goran.rojovic@ethernal.tech>
2024-03-28 16:39:41 +0100

    add migrate down

--- db/migrations/0002.sql
+++ db/migrations/0002.sql
@@ -1,3 +1,6 @@
 -- +migrate Up
 ALTER TABLE state.monitored_txs
 ADD COLUMN num_retries DECIMAL(78, 0) NOT NULL DEFAULT 0;
+
+-- +migrate Down
+ALTER TABLE state.monitored_txs DROP COLUMN num_retries;
