e4a57fc577d36818ae3706a39ca0c5ecc0f12295
Victor Castell <victor@polygon.technology>
2024-04-05 12:08:03 +0200

    Use existing interface

--- types/interfaces.go
+++ types/interfaces.go
@@ -3,7 +3,6 @@ package types
 import (
 	"context"
 	"math/big"
-	"time"
 
 	"github.com/0xPolygon/agglayer/tx"
 	txmTypes "github.com/0xPolygon/agglayer/txmanager/types"
@@ -11,7 +10,6 @@ import (
 	"github.com/0xPolygonHermez/zkevm-node/state"
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
-	gethTypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/jackc/pgx/v4"
 )
 
@@ -23,16 +21,7 @@ type IEtherman interface {
 	GetSequencerAddr(rollupId uint32) (common.Address, error)
 	BuildTrustedVerifyBatchesTxData(lastVerifiedBatch, newVerifiedBatch uint64, proof tx.ZKP, rollupId uint32) (data []byte, err error)
 	CallContract(ctx context.Context, call ethereum.CallMsg, blockNumber *big.Int) ([]byte, error)
-	GetTx(ctx context.Context, txHash common.Hash) (*gethTypes.Transaction, bool, error)
-	GetTxReceipt(ctx context.Context, txHash common.Hash) (*gethTypes.Receipt, error)
-	WaitTxToBeMined(ctx context.Context, tx *gethTypes.Transaction, timeout time.Duration) (bool, error)
-	SendTx(ctx context.Context, tx *gethTypes.Transaction) error
-	PendingNonce(ctx context.Context, account common.Address) (uint64, error)
-	SuggestedGasPrice(ctx context.Context) (*big.Int, error)
-	EstimateGas(ctx context.Context, from common.Address, to *common.Address, value *big.Int, data []byte) (uint64, error)
-	CheckTxWasMined(ctx context.Context, txHash common.Hash) (bool, *gethTypes.Receipt, error)
-	SignTx(ctx context.Context, sender common.Address, tx *gethTypes.Transaction) (*gethTypes.Transaction, error)
-	GetRevertMessage(ctx context.Context, tx *gethTypes.Transaction) (string, error)
+	txmTypes.EthermanInterface
 	GetLastBlock(ctx context.Context, dbTx pgx.Tx) (*state.Block, error)
 }
 
