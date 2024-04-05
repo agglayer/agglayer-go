1d7df9c140b29d7666dbf3ac28577043b9a0c5e3
Victor Castell <0x@vcastellm.xyz>
2024-04-05 10:25:38 +0000

    Add some tests

--- txmanager/types/monitoretx_test.go
+++ txmanager/types/monitoretx_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -35,4 +36,60 @@ func TestTx(t *testing.T) {
 	assert.Equal(t, data, tx.Data())
 	assert.Equal(t, gas+gasOffset, tx.Gas())
 	assert.Equal(t, gasPrice, tx.GasPrice())
+}
+
+func TestAddHistory(t *testing.T) {
+	mTx := MonitoredTx{
+		History:    make(map[common.Hash]bool),
+		NumRetries: 0,
+	}
+
+	tx := types.NewTransaction(0, common.HexToAddress("0x1"), big.NewInt(0), 0, big.NewInt(0), nil)
+
+	err := mTx.AddHistory(tx)
+	assert.NoError(t, err)
+	assert.True(t, mTx.History[tx.Hash()])
+	assert.Equal(t, uint64(0x1), mTx.NumRetries)
+
+	err = mTx.AddHistory(tx)
+	assert.Equal(t, ErrAlreadyExists, err)
+}
+
+func TestMonitoredTx_HistoryStringSlice(t *testing.T) {
+	mTx := MonitoredTx{
+		History: map[common.Hash]bool{
+			common.HexToHash("0x1"): true,
+			common.HexToHash("0x2"): true,
+			common.HexToHash("0x3"): true,
+		},
+	}
+
+	expected := []string{
+		"0x0000000000000000000000000000000000000000000000000000000000000001",
+		"0x0000000000000000000000000000000000000000000000000000000000000002",
+		"0x0000000000000000000000000000000000000000000000000000000000000003",
+	}
+	result := mTx.HistoryStringSlice()
+
+	assert.Equal(t, expected, result)
+}
+
+func TestHistoryHashSlice(t *testing.T) {
+	mTx := MonitoredTx{
+		History: map[common.Hash]bool{
+			common.HexToHash("0x1"): true,
+			common.HexToHash("0x2"): true,
+			common.HexToHash("0x3"): true,
+		},
+	}
+
+	expected := []common.Hash{
+		common.HexToHash("0x1"),
+		common.HexToHash("0x2"),
+		common.HexToHash("0x3"),
+	}
+
+	result := mTx.HistoryHashSlice()
+
+	assert.Equal(t, expected, result)
 }
