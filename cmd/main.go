deff1e88873f1680a6d91f238d13b02b1ff17cb2
Victor Castell <0x@vcastellm.xyz>
2024-04-12 16:39:56 +0000

    Setup the RPC lib logger

--- cmd/main.go
+++ cmd/main.go
@@ -162,6 +162,7 @@ func start(cliCtx *cli.Context) error {
 			},
 		},
 		jRPC.WithHealthHandler(http.HandlerFunc(healthHandler(storage))),
+		jRPC.WithLoggerConfig(log.WithFields("module", "rpc")),
 	)
 
 	// Run RPC
